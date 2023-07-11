e447ce84ec3c7fddbd848ba9c569ca35b2fa3bb2
BrunoSong <brunosong@wjtb.net>
2023-07-11 13:16:14 +0900

    Case

--- src/test/java/com/brunosong/querydsl/QuerydslBasicTest.java
+++ src/test/java/com/brunosong/querydsl/QuerydslBasicTest.java
@@ -6,6 +6,7 @@ import com.brunosong.querydsl.entity.QTeam;
 import com.brunosong.querydsl.entity.Team;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
@@ -575,6 +576,45 @@ public class QuerydslBasicTest {
 
     }
 
+
+
+    @Test
+    public void basicCase() {
+
+
+        List<String> fetch = jpaQueryFactory
+                .select(member.age
+                        .when(10).then("열살")
+                        .when(20).then("이십살")
+                        .otherwise("기타"))
+                .from(member)
+                .fetch();
+
+        for (String s : fetch) {
+            System.out.println(s);
+        }
+
+    }
+
+
+
+    @Test
+    public void complexCase() {
+
+
+        List<String> fetch = jpaQueryFactory
+                .select(new CaseBuilder()
+                        .when(member.age.between(0,20)).then("0~20살")
+                        .when(member.age.between(21,30)).then("21~31살")
+                        .otherwise("기타"))
+                .from(member)
+                .fetch();
+
+        for (String s : fetch) {
+            System.out.println(s);
+        }
+
+    }
 
 
 
